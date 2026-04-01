375c66c3c40d9b9e5272d5ac3e400d0a26c573f9
GuilhermeFontanag <guilhermegregoriofontana>
2026-04-01 16:49:58 -0300

    algo

--- aula_9/src/multa.java
+++ aula_9/src/multa.java
@@ -1,10 +1,8 @@
 public class multa {
     static void main() {
       double velocidade = 0;
-      double multa = 0;
       double valor = 0;
       double excendido = 0;
-      String exibir = "";
       IO.print("informe a sua velocidade (em km)");
       velocidade = Double.parseDouble(IO.readln());
       if (velocidade >80) {
@@ -12,7 +10,7 @@ public class multa {
           excendido = (velocidade - 80);
           valor =  (excendido * 50);
       }
-    IO.println("limite = 80 " +"valor excendido " + String.format("%.1f", excendido) + "valor da multa é "+ valor);
+    IO.println("limite = 80 "+ String.format("%n") +"valor excendido " + String.format("%.0f %n", excendido) + "valor da multa é "+String.format("%.2f %n", valor));
 
 
 
