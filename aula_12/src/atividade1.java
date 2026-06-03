0cd962f04546526b879d43c4c3846657fbd824aa
Guilherme Gregorio <guilhermegregoriofontana@gmail.com>
2026-06-03 14:39:48 -0300

    Delete

--- aula_12/src/atividade1.java
+++
@@ -1,7 +0,0 @@
-public class atividade1 {
-    static void main(String[] args) {
-        for (int i = 15; i<=50; i++){
-            IO.println(i + "^" + "2 = " +  Math.pow(i,2));
-        }
-    }
-}
--- aula_12/src/inteiropar.java
+++
@@ -1,7 +0,0 @@
-public class inteiropar {
-    static void main(String[] args) {
-      for( int i = 1; i <=30; i++){
-          if (i %2 ==0) IO.println("os valores sao "+  i);
-      }
-    }
-}
