600cadb68eca5c533ee01831605681a48e5eea66
GuilhermeFontanag <guilhermegregoriofontana@gmail.com>
2026-06-03 14:36:14 -0300

    teste beta

--- CadastrosAlunos/src/Main.java
+++ CadastrosAlunos/src/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
                 case 1: //O sistema deve permitir cadastrar até 10 alunos.
                     break;
-                    if(nomes.length>= 10) {
+                    if(totalAlunos<=10) {
 
                         for (int i = 0; i >= 10; i++) {
                             //aluno
@@ -30,35 +30,43 @@ public class Main {
                             idades[i] = Integer.parseInt(IO.readln("informe a idade"));
                             //curso
                             cursos[i] = IO.readln("informe o curso");
-                            //para sair
 
+                            totalAlunos++;
                         }
                     }else{
                         IO.println("numero exarcebado");
                     }
                 case 2: //Ao escolher a opção 2, o sistema deve mostrar todos os alunos cadastrados.
-                    break;
-                for(int i = 0 ; i<= idades.length ; i++ ){
+
+                for(int i = 0 ; i<= totalAlunos ; i++ ){
                     IO.println("aluno: " + nomes[i]);
                     IO.println("idade: " + idades[i]);
                     IO.println("curso: " + cursos[i]);
                 }
+                    break;
                 case 3: //Ao escolher a opção 3, o usuário deve digitar um nome e mostra-lo
                     IO.println("informe o nome do aluno");
-
+                    String procura = IO.readln();
+                    for (int i = 0; i<= totalAlunos; i++){
+                        if (nomes[i].equalsIgnoreCase(procura)){
+                            IO.println("nome encontrado: " + nomes[i]);
+                        }else{
+                            IO.println("nome nao escrito");
+                        }
+                    }
                     break;
                 case 4: //Ao escolher a opção 4, o usuário deve digitar o nome do aluno que deseja remover.
-
                     String informe = IO.readln("informe o nome dos alunos");
-                    if (Arrays.asList(informe).contains(nomes)){
-                        IO.println(nomes);
-                    }else{
-                        IO.println("nao existe");
+                    for (int i = 0; i<= totalAlunos; i++){
+                        if (nomes[i].equalsIgnoreCase(informe)){
+                            IO.println("nome removido: " + nomes[i]);
+                            Arrays.asList(nomes[i],idades[i],cursos[i]).remove(i);
+                        }else{
+                            IO.println("nome nao encontrado");
+                        }
                     }
 
-
                     break;
-
                 case 5: //Ao escolher a opção 5, o sistema deve encerrar com uma mensagem:
                     IO.println("Sistema encerrado.");
                     return;
