48dcac137b888126b87239c1996fe374e80e2928
Guilherme Gregorio <guilhermegregoriofontana@gmail.com>
2026-06-08 15:04:16 -0300

    refeito

--- CadastrosAlunos/src/cadastroAluno.java
+++ CadastrosAlunos/src/cadastroAluno.java
@@ -1,79 +1,111 @@
-import java.util.Arrays;
+public class cadastroAluno {
 
-public class CadastroAluno {
-    static void main(String[] args) {
-        //variaveis
+    public static void main(String[] args) {
+
         String[] nomes = new String[10];
         int[] idades = new int[10];
         String[] cursos = new String[10];
         int totalAlunos = 0;
-        //inicio do loop
+
         while (true) {
+
             IO.println("===== SISTEMA DE CADASTRO DE ALUNOS =====");
             IO.println("1 - Cadastrar aluno");
             IO.println("2 - Listar alunos");
             IO.println("3 - Buscar aluno pelo nome");
             IO.println("4 - Remover aluno");
-            IO.println(String.format("5 - Sair %n"));
+            IO.println("5 - Sair");
             IO.println("Escolha uma opção:");
-            int opcaos = Integer.parseInt(IO.readln());
-            switch (opcaos) {
 
-                case 1: //O sistema deve permitir cadastrar até 10 alunos.
-                    break;
-                    if(totalAlunos<=10) {
+            int opcao = Integer.parseInt(IO.readln());
 
-                        for (int i = 0; i >= 10; i++) {
-                            //aluno
-                            nomes[i] = IO.readln("informe o nome do aluno");
-                            //idade
-                            idades[i] = Integer.parseInt(IO.readln("informe a idade"));
-                            //curso
-                            cursos[i] = IO.readln("informe o curso");
+            switch (opcao) {
 
-                            totalAlunos++;
-                        }
-                    }else{
-                        IO.println("numero exarcebado");
+                case 1:
+
+                    if (totalAlunos < 10) {
+
+                        nomes[totalAlunos] = IO.readln("Informe o nome do aluno:");
+                        idades[totalAlunos] = Integer.parseInt(IO.readln("Informe a idade:"));
+                        cursos[totalAlunos] = IO.readln("Informe o curso:");
+                        totalAlunos++;
+                        IO.println("Aluno cadastrado com sucesso!");
+                    } else {
+                        IO.println("Limite de 10 alunos atingido.");
                     }
-                case 2: //Ao escolher a opção 2, o sistema deve mostrar todos os alunos cadastrados.
-
-                for(int i = 0 ; i<= totalAlunos ; i++ ){
-                    IO.println("aluno: " + nomes[i]);
-                    IO.println("idade: " + idades[i]);
-                    IO.println("curso: " + cursos[i]);
-                }
                     break;
-                case 3: //Ao escolher a opção 3, o usuário deve digitar um nome e mostra-lo
-                    IO.println("informe o nome do aluno");
-                    String procura = IO.readln();
-                    for (int i = 0; i<= totalAlunos; i++){
-                        if (nomes[i].equalsIgnoreCase(procura)){
-                            IO.println("nome encontrado: " + nomes[i]);
-                        }else{
-                            IO.println("nome nao escrito");
+                case 2:
+
+                    if (totalAlunos == 0) {
+                        IO.println("Nenhum aluno cadastrado.");
+                    } else {
+                        for (int i = 0; i < totalAlunos; i++) {
+                            IO.println("Aluno: " + nomes[i]);
+                            IO.println("Idade: " + idades[i]);
+                            IO.println("Curso: " + cursos[i]);
+                            IO.println("---------------------");
                         }
                     }
                     break;
-                case 4: //Ao escolher a opção 4, o usuário deve digitar o nome do aluno que deseja remover.
-                    String informe = IO.readln("informe o nome dos alunos");
-                    for (int i = 0; i<= totalAlunos; i++){
-                        if (nomes[i].equalsIgnoreCase(informe)){
-                            IO.println("nome removido: " + nomes[i]);
-                            Arrays.asList(nomes[i],idades[i],cursos[i]).remove(i);
-                        }else{
-                            IO.println("nome nao encontrado");
+                case 3:
+                    if (totalAlunos == 0) {
+                        IO.println("Nenhum aluno cadastrado.");
+                        break;
+                    }
+                    String procura = IO.readln("Informe o nome do aluno:");
+                    boolean encontrado = false;
+                    for (int i = 0; i < totalAlunos; i++) {
+
+                        if (nomes[i].equalsIgnoreCase(procura)) {
+                            IO.println("Aluno encontrado:");
+                            IO.println("Nome: " + nomes[i]);
+                            IO.println("Idade: " + idades[i]);
+                            IO.println("Curso: " + cursos[i]);
+                            encontrado = true;
+                            break;
                         }
                     }
-
+                    if (!encontrado) {
+                        IO.println("Aluno não encontrado.");
+                    }
                     break;
-                case 5: //Ao escolher a opção 5, o sistema deve encerrar com uma mensagem:
+                case 4:
+                    if (totalAlunos == 0) {
+                        IO.println("Nenhum aluno cadastrado.");
+                        break;
+                    }
+                    String nomeRemover = IO.readln("Informe o nome do aluno:");
+                    boolean removido = false;
+
+                    for (int i = 0; i < totalAlunos; i++) {
+                        if (nomes[i].equalsIgnoreCase(nomeRemover)) {
+                            for (int z = 0; z < totalAlunos - 1; z++) {
+                                nomes[z] = nomes[z + 1];
+                                idades[z] = idades[z + 1];
+                                cursos[z] = cursos[z + 1];
+                            }
+                            nomes[totalAlunos - 1] = null;
+                            cursos[totalAlunos - 1] = null;
+                            idades[totalAlunos - 1] = 0;
+                            totalAlunos--;
+                            removido = true;
+
+                            IO.println("Aluno removido com sucesso.");
+                            break;
+                        }
+                    }
+                    if (!removido) {
+                        IO.println("Aluno não encontrado.");
+                    }
+                    break;
+
+                case 5:
                     IO.println("Sistema encerrado.");
                     return;
 
+                default:
+                    IO.println("Opção inválida.");
             }
-
-
         }
     }
 }
